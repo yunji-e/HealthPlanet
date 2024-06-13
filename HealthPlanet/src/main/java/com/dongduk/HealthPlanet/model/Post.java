81064b2bf6eae32d842dcb07641c82f9d200ddfb
goems0 <goems0@naver.com>
2024-06-13 16:40:25 +0900

    update dto using annotation

--- HealthPlanet/src/main/java/com/dongduk/HealthPlanet/model/Post.java
+++ HealthPlanet/src/main/java/com/dongduk/HealthPlanet/model/Post.java
@@ -1,208 +1,59 @@
 package com.dongduk.HealthPlanet.model;
 
+import java.io.Serializable;
+import java.time.LocalDate;
 import java.util.Date;
 
-public class Post {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import jakarta.persistence.Transient;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name="post")
+@Getter @Setter
+public class Post implements Serializable {
+    @Id
+    @SequenceGenerator(name="POST_SEQ_GENERATOR",
+    sequenceName="POST_SEQUENCE", initialValue=100, allocationSize=10)
+    @GeneratedValue(strategy=GenerationType.SEQUENCE,
+    generator="POST_SEQ_GENERATOR")
     private int postid;
     private String title;
     private Date schedule;
+    @Column(name="timezone")
     private String time;
     private String place;
     private int headcount;
     private int cost;
     private String body;
+    @Column(name="viewCount")
     private int view;
     private int wish;
     private int state;
-    private String custid;
+    @Temporal(TemporalType.DATE)
+    private LocalDate posted;
+    private int id;
     private int event;
+    @Transient
+    private String custid;
+    @Transient
     private String sportname;
+    @Transient
     private int applicant;
     
     public Post() {}
 
-    public Post(int postid, String title, Date schedule, String time, String custid) {
-        super();
-        this.postid = postid;
-        this.title = title;
-        this.schedule = schedule;
-        this.time = time;
-        this.custid = custid;
-    }
     
-    public Post(int postid, String title, Date schedule, int state, String custid) {
-        super();
-        this.postid = postid;
-        this.title = title;
-        this.schedule = schedule;
-        this.state = state;
-        this.custid = custid;
-    }
     
-    public Post(int postid, String title, Date schedule, String time, String place, int headcount, int cost,
-            String body, int event) {
-        super();
-        this.postid = postid;
-        this.title = title;
-        this.schedule = schedule;
-        this.time = time;
-        this.place = place;
-        this.headcount = headcount;
-        this.cost = cost;
-        this.body = body;
-        this.event = event;
-    }
-
-    public Post(int postid, String title, Date schedule, String time, String place, int headcount, int cost,
-            String body, String custid, int event) {
-        super();
-        this.postid = postid;
-        this.title = title;
-        this.schedule = schedule;
-        this.time = time;
-        this.place = place;
-        this.headcount = headcount;
-        this.cost = cost;
-        this.body = body;
-        this.custid = custid;
-        this.event = event;
-    }
-
-    public Post(int postid, String title, Date schedule, String place, int headcount, int cost, String body, int view,
-            int wish, int state, String custid, int event) {
-        super();
-        this.postid = postid;
-        this.title = title;
-        this.schedule = schedule;
-        this.place = place;
-        this.headcount = headcount;
-        this.cost = cost;
-        this.body = body;
-        this.view = view;
-        this.wish = wish;
-        this.state = state;
-        this.custid = custid;
-        this.event = event;
-    }
-
-    public int getPostid() {
-        return postid;
-    }
-
-    public void setPostid(int postid) {
-        this.postid = postid;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public Date getSchedule() {
-        return schedule;
-    }
-
-    public void setSchedule(Date schedule) {
-        this.schedule = schedule;
-    }
-
-    public String getPlace() {
-        return place;
-    }
-
-    public void setPlace(String place) {
-        this.place = place;
-    }
-
-    public int getHeadcount() {
-        return headcount;
-    }
-
-    public void setHeadcount(int headcount) {
-        this.headcount = headcount;
-    }
-
-    public int getCost() {
-        return cost;
-    }
-
-    public void setCost(int cost) {
-        this.cost = cost;
-    }
-
-    public String getBody() {
-        return body;
-    }
-
-    public void setBody(String body) {
-        this.body = body;
-    }
-
-    public int getView() {
-        return view;
-    }
-
-    public void setView(int view) {
-        this.view = view;
-    }
-
-    public int getWish() {
-        return wish;
-    }
-
-    public void setWish(int wish) {
-        this.wish = wish;
-    }
-
-    public int getState() {
-        return state;
-    }
-
-    public void setState(int state) {
-        this.state = state;
-    }
-
-    public String getCustid() {
-        return custid;
-    }
-
-    public void setCustid(String custid) {
-        this.custid = custid;
-    }
-
-    public int getEvent() {
-        return event;
-    }
-
-    public void setEvent(int event) {
-        this.event = event;
-    }
-
-    public String getTime() {
-        return time;
-    }
-
-    public void setTime(String time) {
-        this.time = time;
-    }
-
-    public String getSportname() {
-        return sportname;
-    }
-
-    public void setSportname(String sportname) {
-        this.sportname = sportname;
-    }
-
-    public int getApplicant() {
-        return applicant;
-    }
-
-    public void setApplicant(int applicant) {
-        this.applicant = applicant;
-    }
     
+
 }
--- HealthPlanet/src/main/java/com/dongduk/HealthPlanet/model/Wish.java
+++ HealthPlanet/src/main/java/com/dongduk/HealthPlanet/model/Wish.java
@@ -1,24 +1,23 @@
 package com.dongduk.HealthPlanet.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name="wish")
+@Getter @Setter 
 public class Wish {
+    @Id
     private int id;
+    @Id
     private int postid;
     
     public Wish(int id, int postid) {
         super();
         this.id = id;
-        this.postid = postid;
-    }
-    public int getId() {
-        return id;
-    }
-    public void setId(int id) {
-        this.id = id;
-    }
-    public int getPostid() {
-        return postid;
-    }
-    public void setPostid(int postid) {
         this.postid = postid;
     }
     
