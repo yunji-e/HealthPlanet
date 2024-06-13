9b3609e53e6ab7efbb2e7f8914913209d070b09a
goems0 <goems0@naver.com>
2024-06-13 16:49:36 +0900

    change class to interface

---
+++ HealthPlanet/src/main/java/com/dongduk/HealthPlanet/model/dao/UserDao.java
@@ -0,0 +1,32 @@
+package com.dongduk.HealthPlanet.model.dao;
+
+import java.util.List;
+
+import org.springframework.dao.DataAccessException;
+
+import com.dongduk.HealthPlanet.model.*;
+
+public interface UserDao {
+    
+    // 내가 참여한 모임 목록 조회
+    public List<Post> findParticipationList(int id) throws DataAccessException;
+
+    // 나의 찜 목록
+    public List<Post> findWishList(int id) throws DataAccessException;
+    
+    // 내 모임 목록 조회
+    public List<Post> findMyPostList(int id) throws DataAccessException;
+    
+    // 내 모임 조회
+    public Post findMyPost(int id, int postid) throws DataAccessException;
+    
+    // 내 모임 수정(수정하기 버튼 -> 수정페이지)
+    public Post findUpdatePost(int postid) throws DataAccessException;
+    
+    // 내 모임 수정(수정페이지 -> 수정완료)
+    public void UpdatePost(Post post) throws DataAccessException;
+    
+    // 내 모임 삭제
+    public void deletePost(int postid) throws DataAccessException;
+    
+}
