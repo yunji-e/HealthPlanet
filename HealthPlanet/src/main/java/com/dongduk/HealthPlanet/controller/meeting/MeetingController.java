74ffa5e3123c7284d5f299b527d358876be6fa15
jy20240325 <164830914+jy20240325@users.noreply.github.com>
2024-06-20 13:58:10 +0900

    Create MeetingController.java

---
+++ HealthPlanet/src/main/java/com/dongduk/HealthPlanet/controller/meeting/MeetingController.java
@@ -0,0 +1,43 @@
+package com.dongduk.HealthPlanet.controller.meeting;
+
+import com.example.demo.entities.Meeting;
+import com.example.demo.repositories.MeetingRepository;
+
+import jakarta.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/meetings")
+public class MeetingController {
+
+    private final MeetingRepository meetingRepository;
+
+    @Autowired
+    public MeetingController(MeetingRepository meetingRepository) {
+        this.meetingRepository = meetingRepository;
+    }
+
+    @GetMapping("/register")
+    public String showRegistrationForm(Model model) {
+        model.addAttribute("meeting", new Meeting());
+        return "register-meeting";
+    }
+
+    @PostMapping("/register")
+    @Transactional
+    public String registerMeeting(@ModelAttribute Meeting meeting, Model model) {
+        meetingRepository.save(meeting); // Meeting 객체를 저장
+        model.addAttribute("meeting", meeting);
+        return "register-confirm";
+    }
+
+    
+    @GetMapping("/register-finish")
+    public String showRegisterFinishPage() {
+        return "register-finish";
+    }
+}
