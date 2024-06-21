6b4bf3d32244ff0b6f13a6507f9c354d48a51e16
yunji-e <80235305+yunji-e@users.noreply.github.com>
2024-06-21 13:43:12 +0900

    Delete HealthPlanet directory
    path confusing remove

--- HealthPlanet/src/main/java/com/dongduk/HealthPlanet/dao/PostPreviewDao.java
+++
@@ -1,9 +0,0 @@
-package com.dongduk.HealthPlanet.dao;
-
-import java.util.List;
-
-import com.dongduk.HealthPlanet.domain.Post;
-
-public interface PostPreviewDao {
-    List<Post> findPopularPreviewList(String sort, int count) throws Exception;
-}
