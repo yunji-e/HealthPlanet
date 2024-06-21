2bb63501c62a89abfedbb72073f931c258dfc0cc
yunji <yunji@LAPTOP6123>
2024-06-21 14:53:51 +0900

    all post view 구현

--- src/main/java/com/dongduk/HealthPlanet/HomeController.java
+++ src/main/java/com/dongduk/HealthPlanet/HomeController.java
@@ -5,7 +5,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.dongduk.HealthPlanet.dao.jpa.JpaPostDao;
 import com.dongduk.HealthPlanet.dao.jpa.JpaPostPreviewDao;
+import com.dongduk.HealthPlanet.dao.jpa.PostRepository;
 import com.dongduk.HealthPlanet.domain.Post;
 import java.util.List;
 
@@ -13,13 +15,16 @@ import java.util.List;
 public class HomeController {
 
     @Autowired
-//    private MeetingService meetingService;
     private JpaPostPreviewDao postPreviewDao;
+    @Autowired
+    private PostRepository postDao; 
 
     @GetMapping({"/", "/main"})
     public String home(Model model) throws Exception {
         List<Post> popularList = postPreviewDao.findPopularPreviewList("wish", 10);
+        List<Post> postList = postDao.findAll();
         model.addAttribute("popularList", popularList);
+        model.addAttribute("postList", postList);
         return "main"; // main.html 뷰를 반환합니다.
     }
 }
