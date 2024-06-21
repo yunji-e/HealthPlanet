9ee5586f26db88d442cf0abd6fbf4503ae511d05
yunji <yunji@LAPTOP6123>
2024-06-21 20:25:17 +0900

    검색 기능 보완

--- src/main/java/com/dongduk/HealthPlanet/repositories/MeetingRepository.java
+++ src/main/java/com/dongduk/HealthPlanet/repositories/MeetingRepository.java
@@ -1,8 +1,12 @@
 package com.dongduk.HealthPlanet.repositories;
 
 import com.dongduk.HealthPlanet.entities.*;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MeetingRepository extends JpaRepository<Meeting, Long> {
+    List<Meeting> findByEvent(String event);
 }
 
