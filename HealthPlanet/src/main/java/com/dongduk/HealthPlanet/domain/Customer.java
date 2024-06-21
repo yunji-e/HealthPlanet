15de7d3f0b035afa6cb28912ccb3d8c56e7fcedc
yunji <yunji@LAPTOP6123>
2024-06-21 13:12:06 +0900

    master 브랜치 커밋 꼬임 -> 수정

--- HealthPlanet/src/main/java/com/dongduk/HealthPlanet/domain/Customer.java
+++
@@ -1,5 +0,0 @@
-package com.dongduk.HealthPlanet.domain;
-
-public class Customer {
-
-}
--- HealthPlanet/src/main/java/com/dongduk/HealthPlanet/repositories/MeetingRepository.java
+++
@@ -1,8 +0,0 @@
-package com.dongduk.HealthPlanet.repositories;
-
-import com.dongduk.HealthPlanet.entities.*;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MeetingRepository extends JpaRepository<Meeting, Long> {
-}
-
