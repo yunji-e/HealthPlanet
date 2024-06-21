2a1efc82cba943bb9ff3f5d097f3bd780bc6abdf
jy20240325 <164830914+jy20240325@users.noreply.github.com>
2024-06-21 19:12:12 +0900

    Update MeetingController.java

--- src/main/java/com/dongduk/HealthPlanet/controller/meeting/MeetingController.java
+++ src/main/java/com/dongduk/HealthPlanet/controller/meeting/MeetingController.java
@@ -1,41 +1,43 @@
+package com.dongduk.HealthPlanet.controller.meeting;
 
-<!DOCTYPE html>
-<html xmlns:th="http://www.thymeleaf.org">
-<head>
-    <title>Health Planet</title>
-    <style>
-       .title {
-          font-size: 300%;
-       }
-       .subTitle { color: lightgray; }
-    </style>
-    <link rel="stylesheet" th:href="@{/css/cards.css}"/>
-    <script type="text/javascript" th:src="@{/js/cards.js}"></script>
-</head>
-<body>
+import com.dongduk.HealthPlanet.entities.Meeting;
+import  com.dongduk.HealthPlanet.repositories.MeetingRepository;
+
+import jakarta.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/meetings")
+public class MeetingController {
+
+    private final MeetingRepository meetingRepository;
+
+    @Autowired
+    public MeetingController(MeetingRepository meetingRepository) {
+        this.meetingRepository = meetingRepository;
+    }
+
+    @GetMapping("/register")
+    public String showRegistrationForm(Model model) {
+        model.addAttribute("meeting", new Meeting());
+        return "register-meeting";
+    }
+
+    @PostMapping("/register")
+    @Transactional
+    public String registerMeeting(@ModelAttribute Meeting meeting, Model model) {
+        meetingRepository.save(meeting); 
+        model.addAttribute("meeting", meeting);
+        return "register-confirm";
+    }
+
     
-   <h2 th:if="${session.username}">Welcome, <span th:text="${session.username}"></span>!</h2>
-    
-   <!-- nav html 생성 및 삽입 필요 -->
-   
-   <div align="center" style="line-height:50%; margin: 10%;">
-      <h1 class="title">Health Planet</h1>
-      <p class="subTitle">당신의 운동 모임 서비스</p>
-      <br><br><br>
-      <a href="/logout" th:if="${session.username}">Logout</a>
-        <a href="/myPage" th:if="${session.username}">myPage</a>
-        <a href="/signup" th:unless="${session.username}">회원가입</a>
-        <a href="/login" th:unless="${session.username}">로그인</a>
-   </div>
-   
-   <a href="/meetings/search">Search Meetings</a>
-   
-   <a href="/popular/list">실시간 인기 모임</a>
-   <div th:replace="~{postTemplete :: flipCards}"></div>
-   
-   <p>전체 글 view(css 개선 필요)</p>
-   <div th:replace="~{postTemplete :: posts}"></div>
-   <a th:href="@{/meetings/register}"><button>모임 생성하기</button></a>
-   
-</body>
-</html>
+    @GetMapping("/register-finish")
+    public String showRegisterFinishPage() {
+        return "register-finish";
+    }
+}
